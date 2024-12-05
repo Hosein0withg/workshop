a3cd9d8f2542c1e08fc84267062694f367456453
Hosein0withg <hosein.bakhoda2006@gmail.com>
2024-12-05 21:23:06 +0330

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@ int main () {
 printf ( "Hello, World!\n" );
 <<<<<<< HEAD
 =======
-printf("How You Doing?\n");
+printf("How You Doing!?\n");
 >>>>>>> facdfa983d3b8a9362ffa054a1946ffa46798a03
 return 0 ;
 }
